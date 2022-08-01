9782a2bbeac95d79882b2ef5cce7f41b76c0b4c5
Millene Galetti <mikagaletti1@gmail.com>
2022-08-01 19:38:04 -0300

    Feat: Adiciona exercicio 1

---
+++ src/dio/loops/ex01_NomeEIdade.java
@@ -0,0 +1,29 @@
+package dio.loops;
+
+/*
+Faça um programa que leia conjunto de dois valores,
+o primeiro representando o nome do aluno e o segundo a sua idade.
+(Pare o programa inserindo o valor 0 no campo nome)
+ */
+
+import java.util.Scanner;
+
+public class ex01_NomeEIdade {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+     String nome;
+     int idade;
+
+     while(true) {
+         System.out.println("Nome: ");
+         nome = scan.next();
+         if (nome.equals("0")) break;
+
+         System.out.println("Idade: ");
+         idade = scan.nextInt();
+     }
+
+        System.out.println("Continua");
+    }
+}
